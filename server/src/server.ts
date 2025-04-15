b05b5460960885607c8c493ed025051a936954ed
heimsharon <heimsharon@hotmail.com>
2025-04-15 19:29:55 -0400

    adding cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 // Path: server/src/server.ts
 // This file is used to start the server and connect to the database
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -25,6 +26,13 @@ requiredEnvVars.forEach((envVar) => {
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  credentials: true, // Allow cookies if needed
+}));
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
