cc1099e920c6e4ba31222772bcbb561f9e3ed7b4
heimsharon <heimsharon@hotmail.com>
2025-04-15 22:41:00 -0400

    server.ts update for render

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
 
@@ -13,7 +14,8 @@ const requiredEnvVars = [
   'DB_USER',
   'DB_PASSWORD',
   'DB_NAME',
-  'JWT_SECRET_KEY'
+  'JWT_SECRET_KEY',
+  'CLIENT_URL',
 ];
 
 // Check if all required environment variables are set
@@ -29,25 +31,30 @@ const PORT = process.env.PORT || 3001;
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
+  origin: process.env.CLIENT_URL || 'http://localhost:3000', // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true, // Allow cookies if needed
 }));
 
-// Serves static files in the entire client's dist folder
+// Serve static files from the client's dist folder
 app.use(express.static('../client/dist'));
 
 app.use(express.json());
 app.use(routes);
 
-sequelize.
-  sync({ force: false })
+// Handle React routes (fallback to index.html)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist/index.html'));
+});
+
+// Connect to the database and start the server
+sequelize
+  .sync({ force: false })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
     });
   })
-  // Handle 404 errors
   .catch((err: Error) => {
     console.error('Error connecting to the database:', err.message);
     process.exit(1); // Exit the process with an error code
