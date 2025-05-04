a9a0ad259149be807bd4605c172c0e40644d3e7c
heimsharon <heimsharon@hotmail.com>
2025-05-03 22:27:11 -0400

    clean up

--- dist/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, _req, // Prefix with an underscore to suppress the warning
-res, _next // Prefix with an underscore to suppress the warning
-) => {
-    console.error('Error:', err.message);
-    const status = err.status || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(status).json({ error: message });
-};
-export default errorHandler;
